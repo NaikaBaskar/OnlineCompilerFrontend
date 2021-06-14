56f8df940095190e3fed76cb0ca5a10dd332b869
NaikaBaskar <baskarnayak2420@gmail.com>
2021-06-14 23:01:06 +0530

    Replaced buttons with icons

--- js/scripts.js
+++ js/scripts.js
@@ -137,9 +137,10 @@ int main() {
 		lang1='ace/mode/c_cpp'
 		text=`// Online C++ compiler to run C++ program online
 #include <iostream>
+using namespace std;
 int main() {
     // Write C++ code here
-    std::cout << "Hello world!";
+    cout << "Hello world!";
     return 0;
 }`
 
